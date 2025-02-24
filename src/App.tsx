ac6ad43329c798ef3f2c38571a275adab3ed4ee5
Tarık Çekirge <tcekirge@gmail.com>
2025-02-24 17:24:35 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+import { Progress } from "./components/ui/progress"
+
+const App = () => {
+  return (
+    <>
+      <Button >Button</Button>
+      <Progress value={33} />
+    </>
+  )
+}
+
+export default App
