0ffe2f6e25a7a39f9d7f0a53685779cc8d22b8ca
Tarık Çekirge <tcekirge@gmail.com>
2025-02-25 14:56:17 +0300

    added geolocation hook and sketlon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "./context/theme-provider"
 import WeatherDashboard from "./pages/WeatherDashboard"
 import CityPage from "./pages/CityPage"
 import {
-  useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider,
+  QueryClient, QueryClientProvider,
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -1,7 +1,70 @@
+import WeatherSkeleton from "@/components/loading-sketlon"
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert"
+import { Button } from "@/components/ui/button"
+import useGeolocation from "@/hooks/use-geolocation"
+import { AlertTriangle, MapPin, RefreshCcw, Terminal } from "lucide-react"
 
 const WeatherDashboard = () => {
+    const { coordinates, error: locationError, getLocation, isLoading: locationLoading } = useGeolocation()
+    console.log(coordinates)
+
+    const handleRefresh = () => {
+        getLocation();
+        if (coordinates) {
+            //Reload Weather Data
+        }
+    };
+
+    if (locationLoading) {
+        return <WeatherSkeleton />
+    }
+
+
+    if (locationError) {
+        return (
+            <Alert variant={'destructive'}>
+                <AlertTriangle className="h-4 w-4" />
+                <AlertTitle>Location Error!</AlertTitle>
+                <AlertDescription>
+                    <p>{locationError}</p>
+                    <Button onClick={getLocation} variant={'outline'} className="w-fit">
+                        <MapPin className=" size-4" />
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        )
+    }
+
+    if (!coordinates) {
+        return (
+            <Alert variant={'destructive'}>
+                <AlertTriangle className="h-4 w-4" />
+                <AlertTitle>Location Requierd!</AlertTitle>
+                <AlertDescription>
+                    <p>Please enable location access to see your local weather</p>
+                    <Button onClick={getLocation} variant={'outline'} className="w-fit">
+                        <MapPin className=" size-4" />
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        )
+    }
+
     return (
-        <div>weather-dashboard</div>
+        <div className="space-y-4">
+            {/* Favrite cities */}
+            <div className="flex items-center justify-between">
+                <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+                <Button variant={'outline'}
+                    size={'icon'}
+                    onClick={handleRefresh}
+                // disabled={}
+                >
+                    <RefreshCcw className="size-4" />
+                </Button>
+            </div>
+            {/* Current and Hourly wather */}
+        </div>
     )
 }
 
