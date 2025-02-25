4fb71cd11e2323c28c95f058fa0d14be750e2da2
Tarık Çekirge <tcekirge@gmail.com>
2025-02-25 18:54:44 +0300

    Creating Queries for weather API

--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -9,15 +9,15 @@ const WeatherDashboard = () => {
     const { coordinates, error: locationError, getLocation, isLoading: locationLoading } = useGeolocation()
 
     const weatherQuery = useWeatherQuery(coordinates);
-    const locationQuery = useReverseGeocodeQuery(coordinates);
     const forecastQuery = useForecastQuery(coordinates);
+    const locationQuery = useReverseGeocodeQuery(coordinates);
 
     const handleRefresh = () => {
         getLocation();
         if (coordinates) {
-            weatherQuery.refetch()
-            locationQuery.refetch()
-            forecastQuery.refetch()
+            weatherQuery.refetch();
+            forecastQuery.refetch();
+            locationQuery.refetch();
         }
     };
 
@@ -56,6 +56,27 @@ const WeatherDashboard = () => {
         )
     }
 
+    const locationName = locationQuery.data?.[0];
+    if (weatherQuery.error || forecastQuery.error) {
+        return (
+            <Alert variant={'destructive'}>
+                <AlertTriangle className="h-4 w-4" />
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                    <p>Failed to fetch weather dada. PLease try again.</p>
+                    <Button onClick={handleRefresh} variant={'outline'} className="w-fit">
+                        <RefreshCcw className=" size-4" />
+                        retry
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        )
+    }
+
+    if (!weatherQuery.data || !forecastQuery.data) {
+        return <WeatherSkeleton />
+    }
+
     return (
         <div className="space-y-4">
             {/* Favrite cities */}
@@ -64,9 +85,9 @@ const WeatherDashboard = () => {
                 <Button variant={'outline'}
                     size={'icon'}
                     onClick={handleRefresh}
-                // disabled={}
+                    disabled={weatherQuery.isFetching || forecastQuery.isFetching}
                 >
-                    <RefreshCcw className="size-4" />
+                    <RefreshCcw className={`size-4 ${weatherQuery.isFetching ? "animate-spin" : ""}`} />
                 </Button>
             </div>
             {/* Current and Hourly wather */}
