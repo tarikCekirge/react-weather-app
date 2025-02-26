3ee06cdf9f8224cc249cb659ab050fcd0fdf0121
Tarık Çekirge <tcekirge@gmail.com>
2025-02-27 00:04:31 +0300

    progress fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,13 @@
+import { Github } from "lucide-react"
+import { Link } from "react-router-dom"
+
 const Footer = () => {
     return (
         <footer className='py-6 backdrop-blur bg-background/95 supports-[backdrop-filter]:bg-background/60'>
-            <div className="container">
-                Footer
+            <div className="container flex justify-center">
+                <Link target="_blank" to={"https://github.com/tarikCekirge"}>
+                    <Github />
+                </Link>
             </div>
         </footer>
     )
